a3c12dd4653469eb096aaa45c51136f0ea8fea71
ev-moon <xenos96111@gmail.com>
2019-06-29 23:55:32 +0900

    static settings

--- ufo/ufo/settings.py
+++ ufo/ufo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'tutorboard',
     'page',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STAIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
