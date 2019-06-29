d1d3c0b78d03f7a9936e92340f511b5ae29cb1b4
Jongwoo Han <han980817@gmail.com>
2019-06-30 08:12:22 +0900

    Deploy Heroku

--- ufo/ufo/settings.py
+++ ufo/ufo/settings.py
@@ -8,10 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')5_b$@)(s2d@m*l5-19#v%*2-#ebneu7uzt)+c%je)d%*_2$10'
+# SECRET_KEY = ')5_b$@)(s2d@m*l5-19#v%*2-#ebneu7uzt)+c%je)d%*_2$10'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')5_b$@)(s2d@m*l5-19#v%*2-#ebneu7uzt)+c%je)d%*_2$10')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -31,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -113,3 +116,8 @@ STAIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
